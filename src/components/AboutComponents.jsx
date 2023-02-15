aaa009750bbced310d4a89c44ee20a1b918ddbf7
Emilien Gerbouin <emilien.gerbouin@gmail.com>
2023-02-15 10:53:13 +0100

    modify about

--- src/components/AboutComponents.jsx
+++ src/components/AboutComponents.jsx
@@ -3,12 +3,12 @@
 const AboutComponents = () => {
   return (
     <div>
-          <p className="text__of__me">Moi c'est Emilien, j'ai 19 ans et je me suis intéresser au développement web il y a environ 1 ans. <br />
-Au cours de ma première année de BTS NDRC qui ne m'interressait plus, j'ai commencé à me renseigné sur le metier de développeur web et les différentes alternatives aux cours traditionnel (bts, but, ect...). <br />
+          <p className="text__of__me">Moi, c'est Emilien, j'ai 19 ans et je me suis intéressé au développement web il y a environ 1 ans. <br />
+Au cours de ma première année de BTS NDRC qui ne m'interessait plus, j'ai commencé à me renseigner sur le métier de développeur web et les différentes alternatives aux cours traditionnel (bts, but, ect...). <br />
 C'est à ce moment là que j'ai découvert Openclassrooms et leur formation diplomante "Développeur Web". J'ai donc décidé d'arrêter mon BTS pour comment la formation de développeur web. <br />
-Aujourdhui, grace à cette formation j'ai acquis des compétences professionnelles que vous pouvez découvrir dans la partie "compétences", mais aussi des compétences personnelles, comme l'autonimie, la gestion de son temps, ect...
+Aujourd'hui, grâce à cette formation j'ai acquis des compétences professionnelles que vous pouvez découvrir dans la partie "compétences", mais aussi des compétences personnelles, comme l'autonomie, la gestion de son temps, ect...
           </p>
-          <ul className="container__word">
+          {/* <ul className="container__word">
             <li className="wordcloud autonomous">Autonome</li>
             <li className="wordcloud team_spirit">Esprit d'équipe</li>
             <li className="wordcloud serious">Sérieux</li>
@@ -19,7 +19,7 @@ Aujourdhui, grace à cette formation j'ai acquis des compétences professionnell
             <li className="wordcloud respectful">Respectueux</li>
             <li className="wordcloud honest">Honnête</li>
             <li className="wordcloud humor">Humour</li>
-          </ul>
+          </ul> */}
     </div>
   );
 };
